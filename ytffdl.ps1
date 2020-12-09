0625e3b23d1405eae5abf2b4e24b40b2a86ba9c3
Vlad-Florin Chelaru <43094747+vladflorinchelaru@users.noreply.github.com>
2020-12-09 19:27:27 +0200

    Optimize ytffdl.ps1
    Move instead of copy and without weird piping of a character towards a command.

--- ytffdl.ps1
+++ ytffdl.ps1
@@ -1,7 +1,6 @@
 Invoke-WebRequest -Uri https://yt-dl.org/latest/youtube-dl.exe -OutFile youtube-dl.exe
 Invoke-WebRequest -uri https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip -OutFile ffmpeg.zip
 Expand-Archive .\ffmpeg.zip
-xcopy \ffmpeg\ffmpeg-4.3.1-win64-static\bin\ffplay.exe ffplay.exe
-echo F|xcopy.exe .\ffmpeg\ffmpeg-4.3.1-win64-static\bin\ffplay.exe ffplay.exe
+move .\ffmpeg\ffmpeg-*\bin\ffplay.exe ffplay.exe
 Remove-Item .\ffmpeg\ -recurse
 Remove-Item .\ffmpeg.zip -recurse
